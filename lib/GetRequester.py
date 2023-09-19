ebfd2dcd800418dfbd76ac3d643bc3f97ed0ae1d
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-09-19 22:57:45 +0300

    completed the assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response_body = requests.get(self.url)
+        return response_body.content
 
     def load_json(self):
-        pass
+        response_list =[]
+        responses = json.loads(self.get_response_body())
+        for response in responses:
+            response_list.append(response)
+
+        return response_list
+    
+example = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+toprint1 = example.get_response_body()
+print(toprint1)
+
+toprint2 = example.load_json()
+print(toprint2)
